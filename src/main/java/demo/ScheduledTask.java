dd8b52c0a5551e1b66fe1152269d34d5f2bb01f4
George <george.mao@gmail.com>
2014-11-05 14:13:38 -0500

    Add scheduledtask , app properties, and static files

---
+++ src/main/java/demo/ScheduledTask.java
@@ -0,0 +1,23 @@
+package demo;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by George.Mao on 11/5/2014.
+ */
+
+@Component // so our Auto Component scan picks this Component up
+@EnableScheduling
+public class ScheduledTask {
+
+    @Scheduled(fixedDelay = 5000)
+    public void myTask(){
+        System.out.println("task executed at: " + new Date().toString());
+    }
+
+
+}
